426542c890b6ea1c486fc87c5a41769d2c87e067
+ <+>
2023-02-24 22:11:21 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const names = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function() {
+    return names.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function() {
+     return names.slice(2)
+        return returnFirstTwoDrivers.slice[2];
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers] 
+
+function createFareMultiplier(integer) {
+    return function calculate() {
+        return integer*5
+    }
+} 
+const fareDoubler = createFareMultiplier(4)
+const fareTripler = createFareMultiplier(7.2)
+
+function selectDifferentDrivers(names, returnLastTwoDrivers) {
+    return returnLastTwoDrivers(names);
+
+}
